d63d55f8ae458ce52b3789372af2f058e77752e0
unknown <swathy.p16@gmail.com>
2018-05-09 15:22:19 +0530

    Weekly & monthly calorie burn chart

--- src/app/tracker/tracker.component.ts
+++ src/app/tracker/tracker.component.ts
@@ -20,7 +20,9 @@ dayNumber : number;
 weekNumber : number;
 dayIndex: number;
 weekIndex: number;
-dataVal: number[] = [1,1,1,1,1,1,1,1,1,1,1,1];
+dataVal: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+monthDataVal: number[] = [0, 0, 0, 0, 0];
+weekDataVal: number[] = [0, 0, 0, 0, 0, 0, 0];
 yearlyCaloriesBurnt : number = 0;
 monthlyCaloriesBurnt : number = 0;
 weeklyCaloriesBurnt : number = 0;
@@ -28,6 +30,18 @@ chartData:Array<any> = [
     {
       label: 'Yearly Calories Burnt',
       data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+];
+monthlyChartData:Array<any> = [
+    {
+      label: 'Monthly Calories Burnt',
+      data: [0, 0, 0, 0, 0]
+    }
+];
+weeklyChartData:Array<any> = [
+    {
+      label: 'Weekly Calories Burnt',
+      data: [0, 0, 0, 0, 0, 0, 0]
     }
 ];
   constructor(private _workoutTrackerService: WorkoutTrackerService,
@@ -45,11 +59,6 @@ chartData:Array<any> = [
     console.log(e);
   }
   
-    colors = [
-        { 
-        backgroundColor: 'rgba(30, 169, 224, 0.8)'
-        }
-    ]
     chartOptions = {
         responsive: true,
         maintainAspectRatio: false,
@@ -63,6 +72,11 @@ chartData:Array<any> = [
             }] 
           }
     }
+    colors = [
+        { 
+        backgroundColor: 'rgba(30, 169, 224, 0.8)'
+        }
+    ]
     labels =  ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];      
 
     weeklyChartcolor = [
@@ -70,76 +84,48 @@ chartData:Array<any> = [
         backgroundColor: 'rgba(30, 169, 224, 0.8)'
         }
     ]
-    weeklyChartlabels =  ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];      
-    weeklyChartData = [
-        {
-          label: 'Weekly Calories Burnt',
-          data: [21, 56, 4, 31, 45, 15, 57]
-          //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-          //[21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59] 
-         //data: this.calculateMonthlyCalories()
-        }
-    ];
-
+    weeklyChartlabels =  ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];   
     monthlyChartcolor = [
         { 
         backgroundColor: 'rgba(30, 169, 224, 0.8)'
         }
     ]
     monthlyChartlabels =  ['Week1', 'Week2', 'Week3', 'Week4', 'Week5'];      
-    monthlyChartData = [
-        {
-          label: 'Monthly Calories Burnt',
-          data: [21, 56, 4, 31, 45]
-          //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-          //[21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59] 
-         //data: this.calculateMonthlyCalories()
-        }
-    ];
-
-    calculateMonthlyCalories(){
-        //let calories = 0;
-        for(var i=0; i < this.trackerData.monthlyWorkouts.length; i++){
-            this.monthNumber = new Date(this.trackerData.monthlyWorkouts[i].startDate).getMonth();
-            console.log("this.monthNumber : " + this.monthNumber);
-             
-           // this.chartData[0].data[this.monthNumber] = this.chartData[0].data[this.monthNumber] + this.trackerData.monthlyWorkouts[i].caloriesBurnt;
-            //console.log("month name  : " +  )
-        }
-    }
 
     renderChart(){
         for(var i=0; i < this.trackerData.yearlyWorkouts.length; i++){
             this.monthNumber = new Date(this.trackerData.yearlyWorkouts[i].startDate).getMonth();
-            console.log("this.monthNumber : " + this.monthNumber);
-            console.log("Parsed calories: " + Number.parseInt(this.trackerData.yearlyWorkouts[i].caloriesBurnt));
             this.dataVal[this.monthNumber] = this.dataVal[this.monthNumber] + Number.parseInt(this.trackerData.yearlyWorkouts[i].caloriesBurnt);
-            //console.log("month name  : " + this.chartData[0].data[this.monthNumber] );
-            //debugger
             this.yearlyCaloriesBurnt = this.yearlyCaloriesBurnt + Number.parseInt(this.trackerData.yearlyWorkouts[i].caloriesBurnt);
         }
         for(var i=0; i < this.trackerData.weeklyWorkouts.length; i++){
             this.dayNumber = new Date(this.trackerData.weeklyWorkouts[i].startDate).getDay();
-            console.log("this.dayNumber : " + this.dayNumber);
             this.dayIndex = this.dayNumber-1;
-            console.log("this.dayIndex : " + this.dayIndex);
-            this.weeklyChartData[0].data[this.dayIndex] = this.weeklyChartData[0].data[this.dayIndex] + Number.parseInt(this.trackerData.weeklyWorkouts[i].caloriesBurnt);
-            //console.log("month name  : " + this.chartData[0].data[this.monthNumber] );
+            this.weekDataVal[this.dayIndex] = this.weekDataVal[this.dayIndex] + Number.parseInt(this.trackerData.weeklyWorkouts[i].caloriesBurnt);
             this.weeklyCaloriesBurnt = this.weeklyCaloriesBurnt + Number.parseInt(this.trackerData.weeklyWorkouts[i].caloriesBurnt);
         }
         for(var i=0; i < this.trackerData.monthlyWorkouts.length; i++){
             this.weekNumber = Number.parseInt(this.datePipe.transform(new Date(this.trackerData.monthlyWorkouts[i].startDate), 'W'));
-            console.log("this.weekNumber : " + this.weekNumber);
             this.weekIndex = this.weekNumber-1;
-            console.log("this.weekIndex : " + this.weekIndex);
-            this.monthlyChartData[0].data[this.weekIndex] = this.monthlyChartData[0].data[this.weekIndex] + Number.parseInt(this.trackerData.monthlyWorkouts[i].caloriesBurnt);
-            //console.log("month name  : " + this.chartData[0].data[this.monthNumber] );
+            this.monthDataVal[this.weekIndex] = this.monthDataVal[this.weekIndex] + Number.parseInt(this.trackerData.monthlyWorkouts[i].caloriesBurnt);
             this.monthlyCaloriesBurnt = this.monthlyCaloriesBurnt + Number.parseInt(this.trackerData.monthlyWorkouts[i].caloriesBurnt);
         }
         this.chartData = [
             {
               label: 'Yearly Calories Burnt',
               data: this.dataVal 
+            }
+        ];
+        this.monthlyChartData = [
+            {
+              label: 'Montly Calories Burnt',
+              data: this.monthDataVal 
+            }
+        ];
+        this.weeklyChartData = [
+            {
+              label: 'Weekly Calories Burnt',
+              data: this.weekDataVal 
             }
         ];
     }
